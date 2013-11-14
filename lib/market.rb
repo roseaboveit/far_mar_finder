d14d9d03668118aa481b2bc5bfd7acc6a2b97d06
Galina <yermichevagalina@gmail.com>
2013-11-14 11:02:31 -0800

    initial attempt to modify Market search method

--- lib/market.rb
+++ lib/market.rb
@@ -52,17 +52,18 @@ class Market
   end
 
   def self.search(search_term)
-    market_list = []
-    search_regex = Regexp.new(search_term.upcase)
-    all.each do |market_instance|
-      market_instance.to_a.each do |attribute|
-        test = attribute.to_s.upcase.match search_regex
-        if test.class == MatchData
-          market_list << market_instance
-        end 
-      end
-    end
-    market_list
+    all.grep(/search_term/)# search_term is a string, returns empty array
+    # market_list = []
+    # search_regex = Regexp.new(search_term.upcase)
+    # all.each do |market_instance|
+    #   market_instance.to_a.each do |attribute|
+    #     test = attribute.to_s.upcase.match search_regex
+    #     if test.class == MatchData
+    #       market_list << market_instance
+    #     end 
+    #   end
+    # end
+    # market_list
   end
 
   def products #Ideas for EC: Search for impartial name
